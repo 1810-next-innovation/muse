ef7d1df76b148f086a7258662193dbb4116d019c
hisa0131 <biy0ganba@gmail.com>
2018-11-12 02:39:27 +0000

    de

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'carts/show'
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 
   root 'items#top'
   get 'items/about', to: 'items#about'
+  get 'carts/show'
   resources :items, only: [:index, :show, :new, :edit]
 
 
