f1ab462bb69d10aac09cf2b1bea54ee6ea2269a7
hisa0131 <biy0ganba@gmail.com>
2018-11-12 04:11:19 +0000

    contact

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+  resources :contacts
+
+
 
   root 'items#top'
   get 'items/about', to: 'items#about'
