8f60ae679061d17b570b36a9117666fcf8d4a2e3
Nobuya-Yanagisawa <yannob17@gmail.com>
2018-11-12 06:33:39 +0000

    Modify items views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,21 @@ class ItemsController < ApplicationController
 	def show
 	end
 
+	def new
+		@item = Item.new
+	end
+
+	def create
+		item = Item.new(item_params)
+		item.save
+		redirect_to new_item_path
+	end
+
+	def index
+	end
+
+	private
+	def item_params
+  		params.require(:item).permit(:item_name, :stock, :opinion)
+    end
 end
