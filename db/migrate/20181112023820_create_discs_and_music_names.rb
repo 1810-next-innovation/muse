aff9c26eb3d3010fdb33363d969e86b7ebc360c4
yoshiharu2580 <yoshiharu2580@gmail.com>
2018-11-12 04:28:33 +0000

    modify model relation

--- db/migrate/20181112023820_create_discs_and_music_names.rb
+++ db/migrate/20181112023820_create_discs_and_music_names.rb
@@ -1,15 +1,5 @@
 class CreateDiscsAndMusicNames < ActiveRecord::Migration[5.2]
   def change
-  	create_table :discs do |t|
-  		t.string :disc_name
-  		t.timestamps
-  	end
-
-  	create_table :music_names do |t|
-  		t.string :music_name
-  		t.timestamps
-  	end
-
     create_table :discs_music_names, id: false do |t|
     	t.belongs_to :disc,       index: true
     	t.belongs_to :music_name, index: true
