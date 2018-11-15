979184ebfc42eaa04368ecf78c30ef12be6ebdda
hisa0131 <biy0ganba@gmail.com>
2018-11-15 09:32:22 +0000

    最新

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,15 +5,15 @@ class Item < ApplicationRecord
 	def favorited_by?(user)
           favorites.where(user_id: user.id).exists?
     end
-	# has_many :favorites,   dependent: :destroy
-	# has_many :cart_items,  dependent: :destroy
-	# has_many :cart, through: :cart_items
-	# has_many :discs,       dependent: :destroy
-	# has_many :music_names, dependent: :destroy
-	# has_many :reviews,     dependent: :destroy
-	# belongs_to :label
+	has_many :favorites,   dependent: :destroy
+	has_many :cart_items,  dependent: :destroy
+	has_many :cart, through: :cart_items
+	has_many :discs,       dependent: :destroy
+	has_many :music_names, dependent: :destroy
+	has_many :reviews,     dependent: :destroy
+	belongs_to :label
 
-	#validates :label,     presence: true
+	validates :label,     presence: true
 
 	  def self.search(search) 
     if search 
