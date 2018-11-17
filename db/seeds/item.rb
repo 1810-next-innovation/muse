e197a7f6f463a08cc9fcbac49fde1c21a6f1facf
yoshiharu2580 <yoshiharu2580@gmail.com>
2018-11-17 06:05:01 +0000

    seed

---
+++ db/seeds/item.rb
@@ -0,0 +1,22 @@
+# users = []
+# 10.times do |n|
+# 	item_name = Faker::Music.album
+# 	item_image_name = 
+# 	n = 1000
+# 	price_arry = Array.new
+# 	while n <= 3000
+# 		n += 100
+# 		price.push(n)
+# 	end
+# 	price = price_arry.sample
+# 	release_date = Faker::Date.between(20.days.ago, Date.today)
+
+# 	Item.create!(
+# 							label_id: 1,
+# 							item_name: ,
+# 							item_image_name: "",
+# 							price: ,
+# 							release_date: "",
+# 							opinion: "",
+# 							stock: "")
+# end
