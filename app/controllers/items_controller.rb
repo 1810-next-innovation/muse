8aa1ef1b3b7fb20a2da6e902a639faa652f21dbe
hisa0131 <biy0ganba@gmail.com>
2018-11-18 06:56:15 +0000

    favoritshow

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
 		@item = Item.find(params[:id])
 		if @cart_item.blank?
 			puts current_cart.nil?
-  		@cart_item = current_cart.cart_items.build(item_id: params[:item_id])
+  		# @cart_item = current_cart.cart_items.build(item_id: params[:item_id])
   		@review = Review.new 
         @reviews = @item.reviews
   	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,16 @@ class User < ApplicationRecord
   has_many :receivers, 							 dependent: :destroy
   has_many :carts,     							 dependent: :destroy
   has_many :orders, through: :carts, dependent: :destroy
-
+  has_many :favorites
+  has_many :items, through: :favorites
   enum gender: {man: 1, woman: 2 }
-  has_many :favorites, 							 dependent: :destroy
 
-  # validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20, allow_blank: true}
-  # validates :japanese_syllabaries, presence: true, length: { maximum: 30}
-  # validates :gender, acceptance: true
-  # validates :post_code, presence: true, length: { maximum: 7}
-  # validates :address, presence: true
-  # validates :phone_number, presence: true, length: {maximum: 13}
-  # validates :email, email: {allow_blank: true}
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
+  validates :japanese_syllabaries, presence: true, length: { maximum: 30}
+  validates :gender, acceptance: true
+  validates :post_code, presence: true, length: { maximum: 7}
+  validates :address, presence: true
+  validates :phone_number, presence: true, length: {maximum: 13}
 
 
   def self.search(search)
