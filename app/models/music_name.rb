50704c62377b85c47e322bfa212543ae8a4f00d9
Yoshiharu <42399693+yoshiharu2580@users.noreply.github.com>
2018-11-24 13:53:53 +0900

    Update music_name.rb
    修正しました。

--- app/models/music_name.rb
+++ app/models/music_name.rb
@@ -3,7 +3,7 @@ class MusicName < ApplicationRecord
 	belongs_to :artist
 	belongs_to :disc
 
-	# validates :genre_id,  presence: true
-	# validates :artist_id, presence: true
-	# validates :disc_id,   presence: true
+	validates :genre_id,  presence: true
+	validates :artist_id, presence: true
+	validates :disc_id,   presence: true
 end
