71d43d8028f25b6ef269083e99376ea01afc7a50
Nobuya-Yanagisawa <yannob17@gmail.com>
2018-11-28 09:30:57 +0000

    Modify items edit

--- db/migrate/20181111033534_create_items.rb
+++ db/migrate/20181111033534_create_items.rb
@@ -5,7 +5,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.integer :artist_id
       t.integer :genre_id
       t.string :item_name
-      t.text :item_image_id
+      t.string :item_image_id
       t.integer :price
       t.date :release_date
       t.text :opinion
